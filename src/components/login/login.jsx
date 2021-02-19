6f5e8d49bb62e583c1c7d9f5a99b68be823287a7
iamfarooqi <attaurrahmanfarooqi9@gamil.com>
2021-02-19 15:29:55 +0500

    server working with postman

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -8,7 +8,28 @@ import Form from 'react-bootstrap/Form';
 
 
 function Login() {
+
+
     
+    axios({
+        method: 'post',
+        url: url + "/login",
+        data: {
+            email: document.getElementById("login-email").value,
+            password: document.getElementById("login-password").value,
+        }, withCredentials: true
+        
+    }).then((response) => {
+        console.log(response);
+        alert(response.data.message)
+        window.location.href = "home.html"
+    }, (error) => {
+        console.log(error);
+        alert("Please write your correct email and password")
+    });
+    
+    
+
     
     return(
 
@@ -17,11 +38,11 @@ function Login() {
         <Form>
   <Form.Group controlId="formGroupEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Control type="email" id="email" placeholder="Enter email" />
   </Form.Group>
   <Form.Group controlId="formGroupPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
+    <Form.Control type="password" id="password" placeholder="Password" />
   </Form.Group>
 </Form>
 </div>
--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -11,6 +11,31 @@ function Signup() {
 
     
     
+    axios({
+        method: 'post',
+        url: url + "/signup",
+        data: {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            password: document.getElementById("password").value,
+            // phone: document.getElementById("phone").value,
+            // gender: document.getElementById("gender").value
+            
+            
+        },withCredentials: true
+        
+    }).then(function (response) {
+        console.log(response.data.message);
+        alert(response.data.message);
+        window.location.href = "login.html"
+    })
+    
+    .catch(function (error) {
+        console.log(error);
+        alert(response.message)
+    });
+  
+
     
     return(
     
@@ -20,17 +45,17 @@ function Signup() {
 
   <Form.Group controlId="formGroupEmail">
     <Form.Label>Name:</Form.Label>
-    <Form.Control type="name" placeholder="Enter email" />
+    <Form.Control type="name" id="name" placeholder="Enter email" />
   </Form.Group>
 
   <Form.Group controlId="formGroupEmail">
     <Form.Label>Email:</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Control type="email" id="email" placeholder="Enter email" />
   </Form.Group>
 
   <Form.Group controlId="formGroupPassword">
     <Form.Label>Password:</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
+    <Form.Control type="password" id="password" placeholder="Password" />
   </Form.Group>
 
 </Form>
